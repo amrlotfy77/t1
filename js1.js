8f42bf9d019f77947992f709e17524d29966067a
amrlotfy77 <amr_lotfy77@yahoo.com>
2019-01-12 00:07:49 +0200

    sec

--- js1.js
+++ js1.js
@@ -9,15 +9,15 @@ class check{
     pp (){
 
        //alert(this.n.value);                                   //sp[0].innerHTML = fn[0].value
-        for (var i = 0; i < fn.length; i++) {                  //sp[1].innerHTML = fn[1].value
-            sp[i].innerHTML = fn[i].value;
+        for (var i = 0; i < this.n.length; i++) {                  //sp[1].innerHTML = fn[1].value
+            this.v[i].innerHTML = this.n[i].value;
                                                          //sp[2].innerHTML = fn[2].value
         }
                                              //sp[3].innerHTML = fn[3].value
 }
     re(){
 
-        if (fn[3].value != ""){
+        if (this.n[3].value != ""){
 
             var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,30}$/
 
@@ -39,9 +39,9 @@ class check{
 }
 
 fn = document.getElementsByClassName("22")                //fn = document.forms["RegForm"]["fname"]
-f1 = new check(n=fn)
+
 sp = document.getElementsByClassName("11")
-sp1 = new check(v=sp)
+f1 = new check(fn,sp)
 b1 = document.getElementById("b1")
 
 function data(){
